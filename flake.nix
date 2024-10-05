66618c89064831934c61e92e73fce60fae3a48aa
6607changchun <luobojiaozi@163.com>
2024-10-05 09:17:45 +0000

    chore: init

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "timesharingos/clannad";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, ... }: {
+
+    devShells."x86_64-linux".default = let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    pkgs.mkShellNoCC {
+	packages = [pkgs.wget
+		    pkgs.cargo pkgs.rustc pkgs.rustfmt pkgs.gcc
+		    pkgs.openssl pkgs.pkg-config
+		   ];
+    };
+  };
+}
