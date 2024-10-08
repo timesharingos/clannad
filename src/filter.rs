fb78accb895d7c14d34ab26258e7eb5e400bb983
6607changchun <84566142+6607changchun@users.noreply.github.com>
2024-10-08 15:56:22 +0800

    feat: followsymlink (#5)
    * style: format docs
    * feat: symlinkfollowfilter
    * fix(basicFilter): fix symlink lists

--- src/filter.rs
+++ src/filter.rs
@@ -52,7 +52,7 @@ pub trait Filter: IntoIterator<Item = FileInfo> {
 ///
 /// let mut filter = BasicFilter::new(Path::new("resources/normalfolder"));
 /// filter.scan();
-/// assert_eq!(filter.into_iter().len(), 10);
+/// assert_eq!(filter.into_iter().len(), 8 as usize);
 /// ```
 pub struct BasicFilter {
     root: String,
@@ -82,8 +82,23 @@ pub struct BasicFilter {
 /// );
 /// assert_eq!(filter.into_iter().len(), 8 as usize);
 /// ```
-
 pub struct SymlinkFilter {
+    root: String,
+    files: Option<Vec<FileInfo>>,
+}
+
+/// The filter follows the symlink, and transfer all symlink to copy of what it points to.
+///
+/// Symlink Behavior: transfer symlink to destination files/directories, and eliminate symlink.
+///
+/// ```
+/// # use clannad::filter::{Filter, SymlinkFollowFilter};
+/// # use std::path::Path;
+/// let mut filter = SymlinkFollowFilter::new(Path::new("resources/normalsymlink"));
+/// filter.scan();
+/// assert_eq!(filter.into_iter().len(), 10 as usize);
+/// ```
+pub struct SymlinkFollowFilter {
     root: String,
     files: Option<Vec<FileInfo>>,
 }
@@ -111,14 +126,16 @@ impl BasicFilter {
             results.push(FileInfo::new(
                 root_path,
                 root_path,
-                if root_path.is_dir() {
+                if root_path.is_symlink() {
+                    FileType::REGULAR
+                } else if root_path.is_dir() {
                     FileType::DIRECTORY
                 } else {
                     FileType::REGULAR
                 },
                 None,
             ));
-            if root_path.is_file() {
+            if root_path.is_file() || root_path.is_symlink() {
                 continue;
             }
             for subfile in root_path.read_dir().unwrap() {
@@ -267,6 +284,124 @@ impl IntoIterator for SymlinkFilter {
     }
 }
 
+impl Filter for SymlinkFollowFilter {
+    fn new(root: &Path) -> Self {
+        Self {
+            root: root.to_str().expect("invalid path").to_owned(),
+            files: None,
+        }
+    }
+
+    fn scan(&mut self) {
+        self.files = self.list_files();
+    }
+
+    fn files(&self) -> &Option<Vec<FileInfo>> {
+        &self.files
+    }
+
+    fn update(self, root: &Path) -> Self {
+        let mut instance = self;
+        instance.root = root.to_str().expect("invalid path").to_owned();
+        instance.files = None;
+        instance
+    }
+}
+
+impl SymlinkFollowFilter {
+    fn list_files(&self) -> Option<Vec<FileInfo>> {
+        let root = self.root.clone();
+        if !Path::new(&root).try_exists().is_ok_and(|x| x) {
+            return None;
+        }
+        let mut queue = VecDeque::new();
+        let mut results = Vec::new();
+        queue.push_back(root);
+
+        while !queue.is_empty() {
+            let root = queue.pop_front().unwrap();
+            results.push(Self::query_fileinfo(&root));
+            Self::query_next_batch(&root)
+                .iter()
+                .for_each(|p| queue.push_back(p.to_owned()));
+        }
+        Some(results)
+    }
+
+    fn query_next_batch(path: &str) -> Vec<String> {
+        let info = Self::query_fileinfo(path);
+        let abstract_path = Path::new(&info.content_path);
+        if abstract_path.is_file() {
+            vec![]
+        } else {
+            abstract_path
+                .read_dir()
+                .unwrap()
+                .map(|x| x.unwrap())
+                .map(|x| x.path())
+                .map(|p| p.to_str().unwrap().to_owned())
+                .collect()
+        }
+    }
+    fn query_fileinfo(path: &str) -> FileInfo {
+        let abstract_path = Path::new(path);
+        if abstract_path.is_symlink() {
+            Self::follow_link(path)
+        } else {
+            FileInfo::new(
+                abstract_path,
+                abstract_path,
+                if abstract_path.is_dir() {
+                    FileType::DIRECTORY
+                } else {
+                    FileType::REGULAR
+                },
+                None,
+            )
+        }
+    }
+    fn follow_link(symlink: &str) -> FileInfo {
+        let symlink_path = Path::new(symlink);
+        let mut destination_path = symlink_path
+            .read_link()
+            .unwrap()
+            .to_str()
+            .unwrap()
+            .to_owned();
+        while Path::new(&destination_path).is_symlink() {
+            destination_path = Path::new(&destination_path)
+                .read_link()
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .to_owned();
+        }
+        let destination_path = Path::new(symlink_path)
+            .parent()
+            .unwrap()
+            .join(Path::new(&destination_path));
+        FileInfo::new(
+            symlink_path,
+            destination_path.as_path(),
+            if destination_path.is_dir() {
+                FileType::DIRECTORY
+            } else {
+                FileType::REGULAR
+            },
+            None,
+        )
+    }
+}
+
+impl IntoIterator for SymlinkFollowFilter {
+    type Item = FileInfo;
+    type IntoIter = IntoIter<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.files.unwrap_or(Vec::new()).into_iter()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -278,7 +413,7 @@ mod tests {
         assert_eq!(filter.files().is_none(), true);
         filter = filter.update(Path::new("resources/normalfolder"));
         filter.scan();
-        assert_eq!(filter.into_iter().len(), 10 as usize);
+        assert_eq!(filter.into_iter().len(), 8 as usize);
     }
 
     #[test]
@@ -313,5 +448,28 @@ mod tests {
         let mut filter = SymlinkFilter::new(Path::new("resources/normalsymlink"));
         filter.scan();
         assert_eq!(filter.into_iter().len(), 1 as usize);
+    }
+
+    #[test]
+    fn symlink_follow_filter() {
+        let mut filter = SymlinkFollowFilter::new(Path::new("dst"));
+        filter.scan();
+        assert_eq!(filter.files().is_none(), true);
+        filter = filter.update(Path::new("resources/normalfolder"));
+        filter.scan();
+        assert!(filter
+            .files()
+            .as_ref()
+            .unwrap()
+            .iter()
+            .all(|x| x.symlink_path.is_none()));
+        assert_eq!(filter.into_iter().len(), 10 as usize);
+    }
+
+    #[test]
+    fn symlink_follow_filter_symlink_root() {
+        let mut filter = SymlinkFollowFilter::new(Path::new("resources/normalsymlink"));
+        filter.scan();
+        assert_eq!(filter.into_iter().len(), 10 as usize);
     }
 }
